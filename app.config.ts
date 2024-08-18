63fbd2c84ead881ee699216d014e3af15289ed73
hyochan <dooboolab@gmail.com>
2024-08-18 19:18:39 +0900

    1.0.0 (7)

--- app.config.ts
+++ app.config.ts
@@ -22,7 +22,7 @@ if (process.env.STAGE) {
 }
 
 const DEEP_LINK_URL = '[firebaseAppId].web.app';
-const buildNumber = 6;
+const buildNumber = 7;
 
 export default ({config}: ConfigContext): ExpoConfig => ({
   ...config,
