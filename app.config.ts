fb0c9348ac6f6736bfa1ad6e9ae4572ae74a52d9
hyochan <dooboolab@gmail.com>
2024-11-25 01:24:56 +0900

    1.0.4 (22)
    Update packages

--- app.config.ts
+++ app.config.ts
@@ -4,9 +4,25 @@ import type {ConfigContext, ExpoConfig} from '@expo/config';
 import withAndroidLocalizedName from '@mmomtchev/expo-android-localized-app-name';
 
 import {version} from './package.json';
+import dotenv from 'dotenv';
+import {expand} from 'dotenv-expand';
+import path from 'path';
 
 const DEEP_LINK_URL = '[firebaseAppId].web.app';
-const buildNumber = 21;
+const buildNumber = 22;
+
+// https://github.com/expo/expo/issues/23727#issuecomment-1651609858
+if (process.env.STAGE) {
+  expand(
+    dotenv.config({
+      path: path.join(
+        __dirname,
+        ['./.env', process.env.STAGE].filter(Boolean).join('.'),
+      ),
+      override: true,
+    }),
+  );
+}
 
 export default ({config}: ConfigContext): ExpoConfig => ({
   ...config,
